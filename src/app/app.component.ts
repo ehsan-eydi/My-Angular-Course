8ab333ecc57058267dafcb2b76854b08a7a0d81c
ehsan-eydi <architect@intelact.co>
2025-02-01 01:35:52 +0330

    Homework_Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ngcourse';
+
+export class AppComponent implements OnInit, AfterViewInit{
+
+  title = 'My first course 1';
+  description = 'I enjoy to learn Angular';
+
+  constructor(){
+    console.log('I am constructor');
+  }
+
+  ngOnInit(): void {
+  console.log('ngOnInit');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+
+  changeTitle(){
+    this.title = 'My first course 2'
+  }
+
+  changeTitle2(){
+    this.title = 'My first course 3'
+  }
+
+  descriptionUpdate(){
+    this.description = 'Description Updated'
+  }
+
+  vog(){
+    this.description = 'Really??? Who the hell does?'
+  }
+
+  originalDescription(){
+    this.description= 'I enjoy to learn Angular'
+  
+  }
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+
+  count = 3;
+  showFirstExplosion = false;
+  showSecondExplosion = false;
+  showSmile = false;
+ 
+
+    ngOnInit() {
+      const interval = setInterval(() => {
+        if (this.count > 0) {
+          this.count--;
+        } else {
+          clearInterval(interval); // Stop when count reaches 10
+          this.showFirstExplosion = true; // Show explosion when done
+
+          setTimeout(() => {
+            this.showFirstExplosion = false; // Hide explosion after 6s
+            this.showSecondExplosion = true;
+
+            setTimeout(() => {
+              this.showSecondExplosion = false;
+              this.showSmile = true;
+
+              setTimeout(() => {
+                this.showSmile = false;
+              }, 4000)
+            }, 2500)
+          }, 6000);
+        }
+      }, 1000);
+    }
+    //this.startCounting();
+
+/*   startCounting() {
+    this.intervalId = setInterval(() => {
+      if (this.count > 0) {
+        this.count--;
+      } else {
+        clearInterval(this.intervalId); // Stop when count reaches 10
+      }
+    }, 1000); // 1 second interval
+  } */
+}
